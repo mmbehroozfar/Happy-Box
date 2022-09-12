6eef91b62020b27fedfaf4d54c9c8938126a0fec
M.Mahdi Behroozfar <mmbehroozfar@gmail.com>
2022-09-12 19:58:40 +0430

    #2 - Update HappyThing Entity

--- data-model/src/main/java/com/mmb/moveis/data/model/entities/HappyThingEntity.kt
+++ data-model/src/main/java/com/mmb/moveis/data/model/entities/HappyThingEntity.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "happy_things")
 data class HappyThingEntity(
     @PrimaryKey(autoGenerate = true) val id: Long,
+    val name: String,
     @ColumnInfo(name = "created_at") val createdAt: Long,
     @ColumnInfo(name = "modified_at") val modifiedAt: Long,
 )
